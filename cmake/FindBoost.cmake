354dc3a6257be7c7443fe52dd7e54c58cc0dcea8
xinxin <xxx_cumt@163.com>
2023-03-13 22:28:41 +0800

    添加pnp依赖库

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -12,49 +12,49 @@ SET_PROPERTY(CACHE USE_Boost_VERSION PROPERTY STRINGS 1.68)
 
 IF(USE_Boost_VERSION EQUAL 1.68)
 	FIND_PATH(Boost_INCLUDE_DIR NAMES boost-1_68 HINTS ${CMAKE_SOURCE_DIR}/../../SDK/Boost/include)
-	FIND_PATH(Boost_LIB_DIR NAMES libboost_atomic-vc141-mt-gd-x64-1_68.lib HINTS ${CMAKE_SOURCE_DIR}/../../SDK/Boost/lib)
+	FIND_PATH(Boost_LIB_DIR NAMES libboost_atomic-vc140-mt-gd-x64-1_68.lib HINTS ${CMAKE_SOURCE_DIR}/../../SDK/Boost/lib)
 ENDIF()
 
 IF(USE_Boost_VERSION EQUAL 1.68)
   SET(Boost_LIBRARIES
-	libboost_atomic-vc141-mt
-	libboost_chrono-vc141-mt
-	libboost_container-vc141-mt
-	libboost_context-vc141-mt
-	libboost_contract-vc141-mt
-	libboost_coroutine-vc141-mt
-	libboost_date_time-vc141-mt
-	libboost_exception-vc141-mt
-	libboost_fiber-vc141-mt
-	libboost_filesystem-vc141-mt
-	libboost_graph-vc141-mt
-	libboost_iostreams-vc141-mt
-	libboost_locale-vc141-mt
-	libboost_log-vc141-mt
-	libboost_log_setup-vc141-mt
-	libboost_math_c99-vc141-mt
-	libboost_math_c99f-vc141-mt
-	libboost_math_c99l-vc141-mt
-	libboost_math_tr1-vc141-mt
-	libboost_math_tr1f-vc141-mt
-	libboost_math_tr1l-vc141-mt
-	libboost_prg_exec_monitor-vc141-mt
-	libboost_program_options-vc141-mt
-	libboost_random-vc141-mt
-	libboost_regex-vc141-mt
-	libboost_serialization-vc141-mt
-	libboost_signals-vc141-mt
-	libboost_stacktrace_noop-vc141-mt
-	libboost_stacktrace_windbg-vc141-mt
-	libboost_stacktrace_windbg_cached-vc141-mt
-	libboost_system-vc141-mt
-	libboost_test_exec_monitor-vc141-mt
-	libboost_thread-vc141-mt
-	libboost_timer-vc141-mt
-	libboost_type_erasure-vc141-mt
-	libboost_unit_test_framework-vc141-mt
-	libboost_wave-vc141-mt
-	libboost_wserialization-vc141-mt
+	libboost_atomic-vc140-mt
+	libboost_chrono-vc140-mt
+	libboost_container-vc140-mt
+	libboost_context-vc140-mt
+	libboost_contract-vc140-mt
+	libboost_coroutine-vc140-mt
+	libboost_date_time-vc140-mt
+	libboost_exception-vc140-mt
+	libboost_fiber-vc140-mt
+	libboost_filesystem-vc140-mt
+	libboost_graph-vc140-mt
+	libboost_iostreams-vc140-mt
+	libboost_locale-vc140-mt
+	libboost_log-vc140-mt
+	libboost_log_setup-vc140-mt
+	libboost_math_c99-vc140-mt
+	libboost_math_c99f-vc140-mt
+	libboost_math_c99l-vc140-mt
+	libboost_math_tr1-vc140-mt
+	libboost_math_tr1f-vc140-mt
+	libboost_math_tr1l-vc140-mt
+	libboost_prg_exec_monitor-vc140-mt
+	libboost_program_options-vc140-mt
+	libboost_random-vc140-mt
+	libboost_regex-vc140-mt
+	libboost_serialization-vc140-mt
+	libboost_signals-vc140-mt
+	libboost_stacktrace_noop-vc140-mt
+	libboost_stacktrace_windbg-vc140-mt
+	libboost_stacktrace_windbg_cached-vc140-mt
+	libboost_system-vc140-mt
+	libboost_test_exec_monitor-vc140-mt
+	libboost_thread-vc140-mt
+	libboost_timer-vc140-mt
+	libboost_type_erasure-vc140-mt
+	libboost_unit_test_framework-vc140-mt
+	libboost_wave-vc140-mt
+	libboost_wserialization-vc140-mt
 )
 
   SET(Boost_INCLUDE_DIRS ${Boost_INCLUDE_DIR}/boost-1_68)
